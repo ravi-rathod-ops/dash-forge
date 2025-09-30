d4d1e1feff806bb85a7979cc20539a3e23cdb12f
ravi-rathod-ops <workforravirathod@gmail.com>
2025-09-30 14:56:05 +0530

    Setup subdomains in nextjs 14 app in middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
   "/site",
@@ -12,6 +13,49 @@ export default clerkMiddleware(async (auth, req) => {
   const { userId, redirectToSignIn } = await auth();
   if (!userId) {
     return redirectToSignIn({ returnBackUrl: req.url });
+  }
+
+  //
+  // ✅ AFTER AUTH
+  //
+  const url = req.nextUrl;
+  console.log("url", url);
+  const searchParams = url.searchParams.toString();
+  const pathWithSearchParams = `${url.pathname}${
+    searchParams.length > 0 ? `?${searchParams}` : ""
+  }`;
+
+  const hostname = req.headers;
+  const customSubDomain = hostname
+    .get("host")
+    ?.split(process.env.NEXT_PUBLIC_DOMAIN as string)
+    .filter(Boolean)[0];
+
+  if (customSubDomain) {
+    return NextResponse.rewrite(
+      new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
+    );
+  }
+
+  if (url.pathname === "/sign-in" || url.pathname === "/sign-up") {
+    return NextResponse.redirect(new URL(`/agency/sign-in`, req.url));
+  }
+
+  // Rewrite root → /site
+  if (
+    url.pathname === "/" ||
+    (url.pathname === "/site" && url.host === process.env.NEXT_PUBLIC_DOMAIN)
+  ) {
+    return NextResponse.rewrite(new URL("/site", req.url));
+  }
+
+  // Agency, subaccount, payment-successfull rewrites
+  if (
+    url.pathname.startsWith("/agency") ||
+    url.pathname.startsWith("/subaccount") ||
+    url.pathname.startsWith("/payment-successfull")
+  ) {
+    return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url));
   }
 });
 
